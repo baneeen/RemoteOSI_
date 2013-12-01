026cd938b809e6493e33ed50313b6d0094881c92
Baneen <baneeen@hotmail.com>
2013-12-01 13:00:53 -0500

    create new repository

---
+++ OSI/src/Main.java
@@ -0,0 +1,17 @@
+//============================================================================
+//Team Members:
+//Huang, Weijun 
+//AL Mubarak, Baneen
+//CSE 5231	Computer Networks
+// Project
+// Description: this projet is a simplification of OSI Model
+//==========================
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Welcom to OSI Project!");
+		//
+	
+	}
+
+}
