58ec174175f274a8b188d592183c4a36b2c41b21
Baneen <baneeen@hotmail.com>
2013-12-10 13:45:33 -0500

    Testing breaking message >> successed!

--- OSI/src/Main.java
+++ OSI/src/Main.java
@@ -1,3 +1,10 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
 
 //============================================================================
 //Team Members:
@@ -13,8 +20,28 @@ public class Main {
 	public static void main(String[] args) {
 		
 		System.out.println("Welcome");
+		
+		FileInputStream data = openFile("file.txt");
+		
 	
 	}
 
-
+	public static FileInputStream openFile(String fileName){
+		
+		FileInputStream data = null;
+		
+		//find path
+		String filePath = new File(fileName).getAbsolutePath();
+		filePath.concat(fileName);
+		
+		try{
+	         
+	         data = new FileInputStream(filePath);
+		 
+		}catch(Exception ex){
+			 
+	         ex.printStackTrace();
+	      }
+		return data;
+	}
 }
